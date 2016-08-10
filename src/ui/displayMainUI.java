a5f9ee00d41a3666de8ac484755e3334876b9da3
Joseph Wallace <joswall@gmail.com>
2016-08-10 16:00:28 -0400

    Initial ui class added

--- src/ui/displayMainUI.java
+++ src/ui/displayMainUI.java
@@ -1,5 +1,30 @@
 package ui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
 public class displayMainUI {
+	@Override
+    public void start(Stage primaryStage) {
+		
+					
+        primaryStage.setTitle("ICOM 7200 Rig Control");
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 600, 500);
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+        
+    }
 
 }
